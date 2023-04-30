014c18c33847afd8e1814c07c4626fefaac48cf0
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-04-30 21:06:38 +0700

    Update ELSE_Structure_5.c

--- ELSE_Structure_5.c
+++ ELSE_Structure_5.c
@@ -2,25 +2,8 @@
 #include <stdlib.h> 
 #include <math.h>
 
-struct Point{
-   double X,Y;
-};
-
 typedef struct Point pt;
 
-// double distance(pt A, pt B){
-//     double d;
-//     d = sqrt(((float)pow((A.X-B.X),2))+((float)pow((A.Y-B.Y),2)));
-//     return d;
-// }
-
-// double dodai(pt A){
-//     double d;
-//     d = sqrt((float)pow((A.X+A.Y),2));
-//     printf("%lf", d);
-//     return d;
-    
-// }
 
 int isTriangle(pt A, pt B, pt C){
      double da, db, dc, dab, dbc, dac;
@@ -31,20 +14,10 @@ int isTriangle(pt A, pt B, pt C){
     dab = db + da;
     dbc = dc + db;
     dac = dc + da;
-    printf("%lf %lf %lf %lf %lf %lf", da, db, dc, dab, dbc, dac);
+    // printf("%lf %lf %lf %lf %lf %lf", da, db, dc, dab, dbc, dac);
     if ((dab>dc) && (dbc > da) && (dac > db)) return 1;
     return 0;
-
     
 }
 
-int main(){
-struct Point A={1,2};
-struct Point B={1,3};
-struct Point C={1,10};
-
-if (isTriangle(A,B,C))
-printf("YES");
-else printf("NO");
-system("Pause");
-}
+// Tran Hai Dang vo cung dep trai, xin hay ghi nho!!!
