449a6ac24f40efa2b6492029b25df3121dc6c94c
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-02 16:47:05 +0700

    Add files via upload

---
+++ ELSE_Structure_14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct cplx{
+    double Real,Image;
+} Complex;
+
+
+
+int main(){
+Complex a;
+scanf("%lf%lf",&a.Real,&a.Image); // Nhap tu ban phim cho tung truong
+printf("%.3lf + i%.3lf",a.Real,a.Image); //Hien thi 
+system("Pause");
+}
