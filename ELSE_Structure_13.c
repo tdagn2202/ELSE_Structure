d09ee94e7adf89238e8dd4a0163a6fef8e660622
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-02 16:43:30 +0700

    Add files via upload

---
+++ ELSE_Structure_13.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct PhanSo{
+    int TuSo, MauSo;
+} ps;
+
+void inPhanSo(ps a){
+    printf("%d/%d\n", a.TuSo, a.MauSo);
+}
+
+void chuanhoa(ps *pa){
+    int rs,i;
+    for (i=1; i <= pa->TuSo || i<= pa->MauSo ;i++){
+      if ((pa->TuSo%i==0) && (pa->MauSo%i==0)) rs=i;
+    } 
+
+    pa->TuSo /= rs;
+    pa->MauSo /=rs;
+}
+
+void nhan(ps a, ps b, ps*c){
+    ps tich;
+    tich.TuSo = a.TuSo * b.TuSo;
+    tich.MauSo = a.MauSo * b.MauSo;
+    chuanhoa(&tich);
+    *c=tich;
+}
+
+int main(){
+struct PhanSo a = {10,15};
+struct PhanSo b = {2,3};
+struct PhanSo c;
+inPhanSo(a);
+inPhanSo(b);
+nhan(a,b,&c);
+inPhanSo(c);
+system("Pause");
+}
