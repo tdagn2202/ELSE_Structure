43b05c21f4d87ff63c1dc712e67755261f1e758b
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-02 14:27:33 +0700

    Add files via upload

---
+++ ELSE_Structure_11.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct PhanSo{
+    int TuSo, MauSo;
+} ps;
+
+
+
+void inPhanSo(ps a){
+    printf("%d/%d\n", a.TuSo, a.MauSo);
+}
+
+void chuanhoa(ps *pa){
+    int rs,i;
+    for (i=1; i <= pa->TuSo || i<= pa->MauSo ;i++){
+      if ((pa->TuSo%i==0) && (pa->MauSo%i==0)) rs=i;
+    } 
+
+    pa->TuSo /= rs;
+    pa->MauSo /=rs;
+}
+
+int main(){
+struct PhanSo a = {10,15};
+inPhanSo(a);
+chuanhoa(&a);
+inPhanSo(a);
+system("Pause");
+}
