9f05e2f7d13a76423e09610780eb94c1128da510
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-11 09:02:35 +0700

    Update ELSE_Structure_9.c

--- ELSE_Structure_9.c
+++ ELSE_Structure_9.c
@@ -5,8 +5,3 @@ typedef struct PhanSo{
     int TuSo, MauSo;
 }a;
 
-int main(){
-struct PhanSo a;
-scanf("%d%d",&a.TuSo,&a.MauSo); // Nhap tu ban phim cho tung truong
-printf(" %d/%d)",a.TuSo, a.MauSo); //Hien thi 
-}
