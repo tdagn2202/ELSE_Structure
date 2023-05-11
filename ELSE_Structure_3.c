d4cd05b35135b7c7ab0b54bde627468ba73297bb
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-11 09:06:53 +0700

    Update ELSE_Structure_3.c

--- ELSE_Structure_3.c
+++ ELSE_Structure_3.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <math.h>
-struct Point{
-    double x,y;
-};
 
 typedef struct Point pt;
 
 double distance(pt a, pt b){
     double d;
-    d = sqrt(((float)pow((a.x-b.x),2))+((float)pow((a.y-b.y),2)));
+    d = sqrt(((float)pow((a.X-b.X),2))+((float)pow((a.Y-b.Y),2)));
     return d;
 }
