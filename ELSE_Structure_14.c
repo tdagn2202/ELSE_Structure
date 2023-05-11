69a377ffb606ca67be06896a873a8b22dd2cb128
tdagn2202 <124980840+tdagn2202@users.noreply.github.com>
2023-05-11 09:05:51 +0700

    Update ELSE_Structure_14.c

--- ELSE_Structure_14.c
+++ ELSE_Structure_14.c
@@ -4,11 +4,3 @@
 typedef struct cplx{
     double Real,Image;
 } Complex;
-
-
-
-int main(){
-Complex a;
-scanf("%lf%lf",&a.Real,&a.Image); // Nhap tu ban phim cho tung truong
-printf("%.3lf + i%.3lf",a.Real,a.Image); //Hien thi 
-}
